ca468b97bfcb56f43234cc0e5169c4223e2376e7
Daniel Morales <dagamo0605@gmail.com>
2021-09-30 13:59:09 -0500

    Funcionalidades de Menú principal e Información de salud

---
+++ demo/src/main/java/com/example/demo/MenuPrincipalController.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuPrincipalController implements Initializable {
+    @FXML
+    private Button buttonEstudiantes;
+    @FXML
+    private Button buttonTutores;
+    @FXML
+    private Button buttonSalud;
+    @FXML
+    private Button buttonColegios;
+    @FXML
+    private Button buttonInfoAcademica;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    @FXML
+    protected void clickEstudiantes(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void ClickTutores(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void ClickInfoSalud(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void clickColegios(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void ClickInfoAcademica(ActionEvent actionEvent){
+
+    }
+}
